08333e84eac88942fa4ececccb6f7ac4d63fbfcb
hashcode <boniface@hashcode.zm>
2014-07-18 14:34:37 +0200

    Added Question COntroller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the askApp
  */
 angular.module('askApp')
+    .run(function($rootScope) {
+        $rootScope.name = 'You  In';
+    })
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML Boilerplate',
       'Angula',
       'Karma'
     ];
+
+
   });
